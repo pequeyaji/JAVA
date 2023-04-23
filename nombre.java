087cedd991386a1c579755cfec11ff0c8003d7d2
yajaira <yajairapech814@gmail.com>
2023-04-23 17:53:54 -0600

    Agrega el archivo

---
+++ nombre.java
@@ -0,0 +1,9 @@
+
+public class nombre {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println(" Mi nombre  es yajaira guadalupe pech cupul" );
+	}
+
+}
